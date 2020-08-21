03d886bde0b23939feacca0303b562b4b5ac72d2
JeongWonjae <justv95@naver.com>
2020-08-21 20:54:38 +0900

    add .

---
+++ module_project2/config/database-slave.js
@@ -0,0 +1,6 @@
+module.exports={
+  host: 'project-car-slave2.comzsnafugrl.us-east-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'rlatjrwn123',
+  database: 'car'
+}
--- module_project2/index.js
+++ module_project2/index.js
@@ -1,7 +1,9 @@
 const express    = require('express');
 const mysql      = require('mysql');
 const dbconfig   = require('./config/database.js');
+const dbconfigSlave = require('./config/database-slave.js');
 const connection = mysql.createConnection(dbconfig);
+const readConnection = mysql.createConnection(dbconfigSlave);
 const app = express();
 
 app.set('port', process.env.PORT || 8000);
@@ -46,7 +48,7 @@ app.get('/getId', (req, res) => {
   let table=req.param('tName');
   let attribute=req.param('tAttr');
   let length=parseInt(req.param('pLen'));
-  connection.query('select lpad(max(convert(substr('+attribute+','+(length+1)+','+(length+4)+'),signed)+1),4,0) as id from '+table,(error,rows) => {
+  readConnection.query('select lpad(max(convert(substr('+attribute+','+(length+1)+','+(length+4)+'),signed)+1),4,0) as id from '+table,(error,rows) => {
     if (error) throw error;
     console.log('result is : ', rows);
     res.send(rows);
@@ -77,8 +79,16 @@ app.get('/addCar', (req, res) => {
   });
 });
 
+app.get('/customers2/:id', (req, res) => {
+  connection_slave.query('SELECT * from customer WHERE customer_id=\'' + req.params.id + '\'', (error, rows) => {
+    if (error) throw error;
+    console.log('User detail info is: ', rows);
+    res.send(rows);
+  });
+});
+
 app.get('/customers', (req, res) => {
-  connection.query('select * from customer', (error, rows) => {
+  readConnection.query('select * from customer', (error, rows) => {
     if (error) throw error;
     console.log('Customer info is: ', rows);
     res.send(rows);
@@ -87,7 +97,7 @@ app.get('/customers', (req, res) => {
 
 app.get('/customer', (req, res) => {
   let id=req.param('id')
-  connection.query('select * from customer WHERE customer_id=\''+id+'\'', (error, rows) => {
+  readConnection.query('select * from customer WHERE customer_id=\''+id+'\'', (error, rows) => {
     if (error) throw error;
     console.log('Customer info is: ', rows);
     res.send(rows);
@@ -96,7 +106,7 @@ app.get('/customer', (req, res) => {
 
 app.get('/car', (req, res) => {
   let id=req.param('id')
-  connection.query('select * from car_menu WHERE car_id=\''+id+'\'', (error, rows) => {
+  readConnection.query('select * from car_menu WHERE car_id=\''+id+'\'', (error, rows) => {
     if (error) throw error;
     console.log('Car info is: ', rows);
     res.send(rows);
@@ -105,7 +115,7 @@ app.get('/car', (req, res) => {
 
 
 app.get('/cars', (req, res) => {
-  connection.query('SELECT * FROM car_menu WHERE car_id NOT IN(SELECT car_menu_car_id FROM order_details)', (error, rows) => {
+  readConnection.query('SELECT * FROM car_menu WHERE car_id NOT IN(SELECT car_menu_car_id FROM order_details)', (error, rows) => {
     if (error) throw error;
     console.log('Car info is: ', rows);
     res.send(rows);
@@ -114,7 +124,7 @@ app.get('/cars', (req, res) => {
 
 app.get('/salesperson', (req, res) => {
   let id=req.param('id')
-  connection.query('select * from salesperson where salesperson_id=\''+id+'\'', (error, rows) => {
+  readConnection.query('select * from salesperson where salesperson_id=\''+id+'\'', (error, rows) => {
     if (error) throw error;
     console.log('salesperson info is: ', rows);
     res.send(rows);
@@ -123,7 +133,7 @@ app.get('/salesperson', (req, res) => {
 
 app.get('/carprice', (req, res) => {
   let id=req.param('carid')
-  connection.query('select car_price from car_menu where car_id=\''+id+'\'', (error, rows) => {
+  readConnection.query('select car_price from car_menu where car_id=\''+id+'\'', (error, rows) => {
     if (error) throw error;
     console.log('car price is: ',rows);
     res.send(rows);
@@ -154,7 +164,7 @@ app.get('/addInvoice', (req, res) => {
 
 app.get('/lookupInvoice', (req, res) => {
   let invoiceid=req.param('invoiceid');
-  connection.query('SELECT ct.customer_name, cm.car_model, cm.car_color, cm.new_old_state, cm.serial_number, cm.car_year, cm.car_price, sp.salesperson_name FROM customer ct, car_menu cm, sales_invoice si, order_details od, salesperson sp WHERE si.customer_customer_id=ct.customer_id AND si.order_details_order_id=od.order_id AND od.car_menu_car_id=cm.car_id AND sp.salesperson_id=si.salesperson_salesperson_id AND si.invoice_id=\''+invoiceid+'\'',(error,rows) => {
+  readConnection.query('SELECT ct.customer_name, cm.car_model, cm.car_color, cm.new_old_state, cm.serial_number, cm.car_year, cm.car_price, sp.salesperson_name FROM customer ct, car_menu cm, sales_invoice si, order_details od, salesperson sp WHERE si.customer_customer_id=ct.customer_id AND si.order_details_order_id=od.order_id AND od.car_menu_car_id=cm.car_id AND sp.salesperson_id=si.salesperson_salesperson_id AND si.invoice_id=\''+invoiceid+'\'',(error,rows) => {
     if (error) throw error;
     res.send(rows);
   });
@@ -178,7 +188,7 @@ app.get('/cardelete', (req, res) => {
 
 app.get('/serviceChoice/yes', (req, res) => {
   let id=req.param('id');
-  connection.query('SELECT DISTINCT product_name FROM product WHERE service_menu_service_id=\'' + id + '\'', (error, rows) => {
+  readConnection.query('SELECT DISTINCT product_name FROM product WHERE service_menu_service_id=\'' + id + '\'', (error, rows) => {
     if (error) throw error;
     res.send(rows);
   });
@@ -187,7 +197,7 @@ app.get('/serviceChoice/yes', (req, res) => {
 
 app.get('/lookupService', (req, res) => {
   let custid=req.param('custid');
-  connection.query('SELECT * FROM service_records sr LEFT JOIN (SELECT product_id, service_menu_service_id, product_name, parts_name, parts_price from product p1, product_menu pm WHERE p1.parts_id=pm.parts_id) AS p ON sr.product_id=p.product_id LEFT JOIN service_menu sm ON sr.service_id=sm.service_id WHERE sr.customer_customer_id=\''+custid+'\'',(error,rows) => {
+  readConnection.query('SELECT * FROM service_records sr LEFT JOIN (SELECT product_id, service_menu_service_id, product_name, parts_name, parts_price from product p1, product_menu pm WHERE p1.parts_id=pm.parts_id) AS p ON sr.product_id=p.product_id LEFT JOIN service_menu sm ON sr.service_id=sm.service_id WHERE sr.customer_customer_id=\''+custid+'\'',(error,rows) => {
     if (error) throw error;
     res.send(rows);
   });
@@ -195,21 +205,21 @@ app.get('/lookupService', (req, res) => {
 
 app.get('/lookupMechanic', (req, res) => {
   let serviceid=req.param('serviceid');
-  connection.query('SELECT mechanic_name FROM management m, mechanic mc WHERE m.mechanic_mechanic_id=mc.mechanic_id AND m.service_record_id=\''+serviceid+'\'',(error,rows) => {
+  readConnection.query('SELECT mechanic_name FROM management m, mechanic mc WHERE m.mechanic_mechanic_id=mc.mechanic_id AND m.service_record_id=\''+serviceid+'\'',(error,rows) => {
     if (error) throw error;
     res.send(rows);
   });
 });
 
 app.get('/service', (req, res) => {
-  connection.query('select * from service_menu', (error, rows) => {
+  readConnection.query('select * from service_menu', (error, rows) => {
     if (error) throw error;
     res.send(rows);
   });
 });
 
 app.get('/lookupMechanics', (req, res) => {
-  connection.query('select * from mechanic', (error, rows) => {
+  readConnection.query('select * from mechanic', (error, rows) => {
     if (error) throw error;
     res.send(rows);
   });
@@ -217,7 +227,7 @@ app.get('/lookupMechanics', (req, res) => {
 
 app.get('/lookupTicket', (req, res) => {
   let custid=req.param('custid');
-  connection.query('select service_ticket_id from service_records where customer_customer_id=\''+custid+'\'',(error,rows) => {
+  readConnection.query('select service_ticket_id from service_records where customer_customer_id=\''+custid+'\'',(error,rows) => {
     if (error) throw error;
     res.send(rows);
   });
@@ -226,7 +236,7 @@ app.get('/lookupTicket', (req, res) => {
 
 app.get('/serviceChoice', (req, res) => {
   let id=req.param('id');
-  connection.query('select need_product from service_menu where service_id=\'' + id + '\'', (error, rows) => {
+  readConnection.query('select need_product from service_menu where service_id=\'' + id + '\'', (error, rows) => {
     if (error) throw error;
     res.send(rows);
   });
@@ -235,7 +245,7 @@ app.get('/serviceChoice', (req, res) => {
 
 app.get('/checkTicket', (req, res) => {
   let ticket=req.param('ticket');
-  connection.query('select service_ticket_id from service_records where service_ticket_id=\''+ticket+'\'',(error,rows) => {
+  readConnection.query('select service_ticket_id from service_records where service_ticket_id=\''+ticket+'\'',(error,rows) => {
     if (error) throw error;
     res.send(rows);
   });
@@ -277,7 +287,7 @@ app.get('/addManagement', (req, res) => {
 
 app.get('/service/getMecanic', (req, res) => {
   let serviceid=req.param('serviceid');
-  connection.query('SELECT mc.mechanic_name from management ma, mechanic mc where ma.mechanic_mechanic_id=mc.mechanic_id AND service_record_id=\''+serviceid+'\'',(error,rows) => {
+  readConnection.query('SELECT mc.mechanic_name from management ma, mechanic mc where ma.mechanic_mechanic_id=mc.mechanic_id AND service_record_id=\''+serviceid+'\'',(error,rows) => {
     if (error) throw error;
     res.send(rows);
   });
@@ -286,7 +296,7 @@ app.get('/service/getMecanic', (req, res) => {
 
 app.get('/service/getName', (req, res) => {
   let serviceid=req.param('serviceid');
-  connection.query('SELECT sm.service_name FROM service_menu sm, service_records sr WHERE sm.service_id=sr.service_id AND sr.service_record_id=\''+serviceid+'\'',(error,rows) => {
+  readConnection.query('SELECT sm.service_name FROM service_menu sm, service_records sr WHERE sm.service_id=sr.service_id AND sr.service_record_id=\''+serviceid+'\'',(error,rows) => {
     if (error) throw error;
     res.send(rows);
   });
@@ -294,7 +304,7 @@ app.get('/service/getName', (req, res) => {
 
 app.get('/service/getCustName', (req, res) => {
   let serviceid=req.param('serviceid');
-  connection.query('SELECT c.customer_name FROM customer c, service_records sr WHERE c.customer_id=sr.customer_customer_id AND sr.service_record_id=\''+serviceid+'\'',(error,rows) => {
+  readConnection.query('SELECT c.customer_name FROM customer c, service_records sr WHERE c.customer_id=sr.customer_customer_id AND sr.service_record_id=\''+serviceid+'\'',(error,rows) => {
     if (error) throw error;
     res.send(rows);
   });
@@ -302,7 +312,7 @@ app.get('/service/getCustName', (req, res) => {
 
 app.get('/parts', (req, res) => {
   let parts_name=req.param('parts_name');
-  connection.query('select * from product_menu where parts_name like\'' + parts_name + '\'', (error, rows) => {
+  readConnection.query('select * from product_menu where parts_name like\'' + parts_name + '\'', (error, rows) => {
     if (error) throw error;
     res.send(rows);
   });
@@ -310,14 +320,14 @@ app.get('/parts', (req, res) => {
 
 app.get('/product', (req, res) => {
   let id=req.param('id');
-  connection.query('SELECT parts_name FROM product p, product_menu pm WHERE p.parts_id=pm.parts_id and p.parts_id=\'' + id + '\'', (error, rows) => {
+  readConnection.query('SELECT parts_name FROM product p, product_menu pm WHERE p.parts_id=pm.parts_id and p.parts_id=\'' + id + '\'', (error, rows) => {
     if (error) throw error;
     res.send(rows);
   });
 });
 
 app.get('/mechanic', (req, res) => {
-  connection.query('select * from mechanic', (error, rows) => {
+  readConnection.query('select * from mechanic', (error, rows) => {
     if (error) throw error;
     res.send(rows);
   });
@@ -325,7 +335,7 @@ app.get('/mechanic', (req, res) => {
 
 app.get('/mechanicChoice', (req, res) => {
   let id=req.param('id');  
-  connection.query('select mechanic_name from mechanic where mechanic_id=\'' +id+ '\'', (error, rows) => {
+  readConnection.query('select mechanic_name from mechanic where mechanic_id=\'' +id+ '\'', (error, rows) => {
     if (error) throw error;
     res.send(rows);
   });
